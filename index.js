a7b5c3217c203d44179086ff6a9beaf926ce12fb
shokhDeveloper <shohijahonmusinkulov@gmail.com>
2025-12-15 14:37:44 +0500

    Add index.js codes

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Github nima ?
 // Git
+
+class Car {
+    constructor(carname, caryear){
+        this.carname = carname;
+        this.caryear = caryear
+    }
+};
+
+let cobalt = new Car('Cobalt', 2025);
+console.log(cobalt)
+
+// main (local)
+// main (global)
